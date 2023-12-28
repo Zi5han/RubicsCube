5aa417923f19ae9c4b2df4a08af34683fdf4e823
Zi5han <84861306+Zi5han@users.noreply.github.com>
2023-12-28 03:17:05 +0100

    Fixed Typo

--- RubicsCubeProject/AbstractGameInterface.h
+++ RubicsCubeProject/AbstractGameInterface.h
@@ -6,7 +6,7 @@ class AbstractGameInterface {
 public:
 	virtual void Initialize() {}
 	virtual void Initialize(GLFWwindow* window) { Initialize(); }
-	virtual void RenderInterface(float aspectRation) {}
+	virtual void RenderInterface(float aspectRatio) {}
 	virtual void Update(double deltaTime) {}
 	virtual void ClearResources() {}
 };
