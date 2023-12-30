cf6b9121165b7aa2dbd572e6d92debeda42e9425
Zi5han <84861306+Zi5han@users.noreply.github.com>
2023-12-30 05:21:55 +0100

    Added Cubie

---
+++ RubicsCubeProject/Cubie.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+
+#include <vector>
+
+struct Cubie {
+	int m_number;
+	glm::vec3 m_position = glm::vec3(0.0f);
+	glm::mat4 m_visibleRotation = glm::mat4(1.0f);
+	glm::mat4 m_snapedRotation = glm::mat4(1.0f);
+};
