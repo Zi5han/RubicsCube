fe7df285596dbdb75a86dd890e2020a2bbd8d3c6
Zi5han <84861306+Zi5han@users.noreply.github.com>
2023-12-30 05:23:41 +0100

    Updated RubicsCube
    Added Slice Enum
    Model transformation is now Cubie dependent
    Added more preparation for Animation

--- RubicsCubeProject/RubicsCube.cpp
+++ RubicsCubeProject/RubicsCube.cpp
@@ -5,22 +5,29 @@
 void RubicsCube::Initialize() {
 	m_cubieRenderer.Initialize();
 	d_lr.Initialize();
+
+	float offset = m_cubieRenderer.GetCubieExtention();
+
+	for (int z = 0; z < 3; z++)
+		for (int y = 0; y < 3; y++)
+			for (int x = 0; x < 3; x++) {
+				m_grid[x][y][z] = new Cubie;
+				m_grid[x][y][z]->m_number = x + y + z;
+				m_grid[x][y][z]->m_position = glm::vec3((x - 1) * offset, (y - 1) * offset, (z - 1) * offset);
+			}
+
 }
 
 void RubicsCube::Render(const glm::mat4& viewProjection) {
 	//Offset + 0.1f damit die Luecken zwischen den Minicubies erscheinen.
-	float offset = m_cubieRenderer.GetCubieExtention() + 0.1f;
-	for (int i = 0; i < 3; ++i) {
-		for (int j = 0; j < 3; ++j) {
-			for (int k = 0; k < 3; ++k) {
-				glm::mat4 model = glm::mat4_cast(m_modelRotation);
-				model = glm::translate(model, glm::vec3((i - 1) * offset, (j - 1) * offset, (k - 1) * offset)); // then apply translation
+	glm::mat4 cubeRotation = glm::mat4_cast(m_modelRotation);
 
-				//Rotation der mittleren horizontalen auf jeder Seite
-				//compound = glm::rotate(compound, glm::radians(90.0f) * (i % 2), glm::vec3(1.0f, 0.0f, 0.0f));
-				//compound = glm::rotate(compound, glm::radians(90.0f) * (j % 2), glm::vec3(0.0f, 1.0f, 0.0f));
-				//compound = glm::rotate(compound, glm::radians(90.0f) * (k % 2), glm::vec3(0.0f, 0.0f, 1.0f));
-				m_cubieRenderer.Render(viewProjection, model);
+	for (int z = 0; z < 3; z++) {
+		for (int y = 0; y < 3; y++) {
+			for (int x = 0; x < 3; x++) {
+				glm::mat4 cubieTranslation = glm::translate(glm::mat4(1.0f), m_grid[x][y][z]->m_position);
+				glm::mat4 cubieRotation = m_grid[x][y][z]->m_visibleRotation;
+				m_cubieRenderer.Render(viewProjection, cubeRotation * cubieRotation * cubieTranslation);
 			}
 		}
 	}
@@ -33,42 +40,78 @@ void RubicsCube::Render(const glm::mat4& viewProjection) {
 }
 
 void RubicsCube::Update(const GameInterface& gameInterface) {
-	if (m_animationState == SNAPING)
-		m_animationState = STABLE;
+	//Animation
+	if (m_animationState == SNAPING) {
+		h_UpdateAnimation(gameInterface);
+	}
 
 	d_gameInterface = &gameInterface;
-	m_inputSystem = &gameInterface.GetInputComponent();
 
-	if (m_inputSystem->GetActiveMouseButton() == InputSystem::RIGHT_BUTTON) {
-		if (m_inputSystem->GetRightClickState() == InputSystem::HOLD) {
+	h_UpdateMouse(gameInterface);
+	h_UpdateKeyInput(gameInterface);
+
+	m_previousScreenPosition = inputSystem->GetScreenPosition();
+}
+
+void RubicsCube::h_UpdateMouse(const GameInterface& gameInterface)
+{
+	inputSystem = &gameInterface.GetInputComponent();
+
+	if (inputSystem->GetActiveMouseButton() == InputSystem::RIGHT_BUTTON) {
+		if (inputSystem->GetRightClickState() == InputSystem::HOLD) {
 			h_RotateCube(gameInterface);
 		}
 	}
-	else if (m_inputSystem->GetActiveMouseButton() == InputSystem::LEFT_BUTTON) {
-		if (m_inputSystem->GetLeftClickState() == InputSystem::CLICK) {
-			if (m_animationState == STABLE) {
+	else if (inputSystem->GetActiveMouseButton() == InputSystem::LEFT_BUTTON) {
+		switch (inputSystem->GetLeftClickState()) {
 
-				h_DetermineClickedFace(gameInterface);
-				m_animationState = ROTATING;
+		case InputSystem::CLICK:
+			if (m_animationState != STABLE)
+				break;
+			h_DetermineClickedFace(gameInterface);
+			m_animationState = ROTATING;
+			break;
+
+		case InputSystem::HOLD:
+			if (m_animationState == SNAPING)
+				break;
+			if (m_animationState == ROTATING) {
+				if (m_activeFaceSlice == UNSET_SLICE)
+					if (glm::length(inputSystem->GetScreenPosition() - inputSystem->GetDragStartScreenPosition()) > 100) {
+						h_DetermineActiveFace(gameInterface);
+						break;
+					}
+				h_RotateFace(gameInterface);
 			}
-		}
-		if (m_inputSystem->GetLeftClickState() == InputSystem::RELEASE) {
+			break;
+
+		case InputSystem::RELEASE:
 			m_animationState = SNAPING;
-			return;
+			break;
+
+		default:
+			break;
+
 		}
 	}
-	// Safe previous screen position
-	m_previousScreenPosition = m_inputSystem->GetScreenPosition();
+}
+
+void RubicsCube::h_UpdateKeyInput(const GameInterface& gameInterface) {
+	// TODO
 }
 
 void RubicsCube::ClearResources() {
 	m_cubieRenderer.ClearResources();
+	for (auto& arr2d : m_grid)
+		for (auto& arr : arr2d)
+			for (auto& e : arr)
+				delete e;
 }
 
 //HELPING METHODS
 void RubicsCube::h_RotateCube(const GameInterface& gameInterface) {
 	// Calculate the difference between the current and previous screen positions
-	glm::vec2 delta = m_inputSystem->GetScreenPosition() - m_previousScreenPosition;
+	glm::vec2 delta = inputSystem->GetScreenPosition() - m_previousScreenPosition;
 
 	// Convert the delta vector to radians
 	float angleX = glm::radians(delta.x);
@@ -78,7 +121,7 @@ void RubicsCube::h_RotateCube(const GameInterface& gameInterface) {
 	glm::quat rotationX = glm::angleAxis(angleX, glm::vec3(0.0f, 1.0f, 0.0f));
 	glm::quat rotationY = glm::angleAxis(angleY, glm::vec3(1.0f, 0.0f, 0.0f));
 
-	// Apply the rotations to the model
+	// Apply the rotations to the cubeRotation
 	m_modelRotation = rotationX * m_modelRotation;
 	m_modelRotation = rotationY * m_modelRotation;
 	m_modelRotation = glm::normalize(m_modelRotation);
@@ -86,10 +129,9 @@ void RubicsCube::h_RotateCube(const GameInterface& gameInterface) {
 
 void RubicsCube::h_DetermineClickedFace(const GameInterface& gameInterface) {
 	glm::vec3 origin, direction;
-	m_inputSystem->GetPickingRay(origin, direction);
+	inputSystem->GetPickingRay(origin, direction);
 
-	//Potentielle
-	float planeOffset = 1.5f + m_cubieRenderer.GetCubieExtention();
+	float planeOffset = 1.5f * m_cubieRenderer.GetCubieExtention();
 	for (auto normal : NORMALS_OF_FACES) {
 		const glm::vec3 faceNormal = glm::mat3_cast(m_modelRotation) * normal.second;
 		if (glm::dot(direction, faceNormal) > 0)
@@ -117,21 +159,68 @@ void RubicsCube::h_DetermineClickedFace(const GameInterface& gameInterface) {
 			continue;
 
 		m_clickedFace = normal.first;
-		std::cout << m_clickedFace << ' ' <<
-			d_endPoint.x << ' ' <<
-			d_endPoint.y << ' ' <<
-			d_endPoint.z << '\n';
+		m_rayCubeIntersectionPoint = intersectionPoint;
 		d_endPoint = intersectionPointInObjectSpace;
+		//std::cout << m_clickedFace << ' ' <<
+		//	d_endPoint.x << ' ' <<
+		//	d_endPoint.y << ' ' <<
+		//	d_endPoint.z << '\n';
 		break;
 	}
 }
 
+void RubicsCube::h_RotateFace(const GameInterface& gameInterface) {
+
+}
+
+void RubicsCube::h_DetermineActiveFace(const GameInterface& gameInterface) {
+	glm::vec3 intersectionPointInObjectSpace
+		= glm::inverse(glm::mat3_cast(m_modelRotation)) * m_rayCubeIntersectionPoint;
+	float planeOffset = 1.5f * m_cubieRenderer.GetCubieExtention();
+
+	//int activeSliceX = static_cast<int>((intersectionPointInObjectSpace.x));
+	//int activeSliceY = static_cast<int>((intersectionPointInObjectSpace.x));
+	//int activeSliceZ = static_cast<int>((intersectionPointInObjectSpace.x));
+
+	//float activeSliceX = intersectionPointInObjectSpace.x;
+	//float activeSliceY = intersectionPointInObjectSpace.y;
+	//float activeSliceZ = intersectionPointInObjectSpace.z;
+	//
+	//if (m_clickedFace == TOP_FACE || m_clickedFace == BOTTOM_FACE) {
+	//	//m_activeCube = activeSliceX;
+	//}
+	//else if (m_clickedFace == LEFT_FACE || m_clickedFace == RIGHT_FACE) {
+	//	//m_activeCube = activeSliceY;
+	//}
+	//else { // m_clickedFace == FRONT || m_clickedFace == BACK
+	//	//m_activeCube = activeSliceZ;
+	//}
+	//
+	//std::cout << activeSliceX << ' ' << activeSliceY << ' ' << activeSliceZ << ' ' << '\n';
+}
+
+void RubicsCube::h_UpdateAnimation(const GameInterface& gameInterface) {
+	m_animationState = STABLE;
+}
+
 //STATIC
-const std::map<RubicsCube::Face, glm::vec3> RubicsCube::NORMALS_OF_FACES = {
-	   {RubicsCube::FRONT, glm::vec3(0.0f, 0.0f, 1.0f)},
-	   {RubicsCube::RIGHT, glm::vec3(1.0f, 0.0f, 0.0f)},
-	   {RubicsCube::TOP, glm::vec3(0.0f, 1.0f, 0.0f)},
-	   {RubicsCube::BACK, glm::vec3(0.0f, 0.0f, -1.0f)},
-	   {RubicsCube::LEFT, glm::vec3(-1.0f, 0.0f, 0.0f)},
-	   {RubicsCube::BOTTOM, glm::vec3(0.0f, -1.0f, 0.0f)}
+const std::map<RubicsCube::CubeFace, glm::vec3> RubicsCube::NORMALS_OF_FACES = {
+	   {RubicsCube::RIGHT_FACE,		glm::vec3(1.0f, 0.0f, 0.0f)},
+	   {RubicsCube::TOP_FACE,		glm::vec3(0.0f, 1.0f, 0.0f)},
+	   {RubicsCube::FRONT_FACE,		glm::vec3(0.0f, 0.0f, 1.0f)},
+	   {RubicsCube::LEFT_FACE,		glm::vec3(-1.0f, 0.0f, 0.0f)},
+	   {RubicsCube::BOTTOM_FACE,	glm::vec3(0.0f, -1.0f, 0.0f)},
+	   {RubicsCube::BACK_FACE,		glm::vec3(0.0f, 0.0f, -1.0f)}
+};
+
+const std::map<RubicsCube::CubeSlice, glm::vec3> RubicsCube::NORMALS_OF_SLICES = {
+	   {RubicsCube::RIGHT_SLICE,	glm::vec3(1.0f, 0.0f, 0.0f)},
+	   {RubicsCube::TOP_SLICE,		glm::vec3(0.0f, 1.0f, 0.0f)},
+	   {RubicsCube::FRONT_SLICE,	glm::vec3(0.0f, 0.0f, 1.0f)},
+	   {RubicsCube::LEFT_SLICE,		glm::vec3(1.0f, 0.0f, 0.0f)},
+	   {RubicsCube::BOTTOM_SLICE,	glm::vec3(0.0f, 1.0f, 0.0f)},
+	   {RubicsCube::BACK_SLICE,		glm::vec3(0.0f, 0.0f, 1.0f)},
+	   {RubicsCube::X_MID_SLICE,	glm::vec3(1.0f, 0.0f, 0.0f)},
+	   {RubicsCube::Y_MID_SLICE,	glm::vec3(0.0f, 1.0f, 0.0f)},
+	   {RubicsCube::Z_MID_SLICE,	glm::vec3(0.0f, 0.0f, 1.0f)},
 };
--- RubicsCubeProject/RubicsCube.h
+++ RubicsCubeProject/RubicsCube.h
@@ -2,11 +2,13 @@
 #include "CubieRenderer.h"
 #include "LineRenderer.h"
 #include "InputSystem.h"
+#include "Cubie.h";
 
 #include <iostream>
 #include <map>
 #include <string>
 #include <vector>
+#include <array>
 
 #include <glm/ext.hpp>
 
@@ -21,44 +23,73 @@ public:
 	};
 
 	enum Axis {
+		UNSET_AXIS = -1,
 		X = 0,
 		Y = 1,
 		Z = 2
 	};
 
-	enum Face {
-		UNSET = -1,
-		FRONT = 0,
-		RIGHT = 1,
-		TOP = 2,
-		BACK = 3,
-		LEFT = 4,
-		BOTTOM = 5
+	enum CubeFace {
+		UNSET_FACE = -1,
+		RIGHT_FACE = 0,
+		TOP_FACE = 1,
+		FRONT_FACE = 2,
+		LEFT_FACE = 3,
+		BOTTOM_FACE = 4,
+		BACK_FACE = 5
 	};
+
+	enum CubeSlice {
+		UNSET_SLICE = -1,
+		RIGHT_SLICE = 0,
+		TOP_SLICE = 1,
+		FRONT_SLICE = 2,
+		LEFT_SLICE = 3,
+		BOTTOM_SLICE = 4,
+		BACK_SLICE = 5,
+		X_MID_SLICE = 6,
+		Y_MID_SLICE = 7,
+		Z_MID_SLICE = 8
+	};
+
 	void Initialize();
 	void Render(const glm::mat4& viewProjection);
 	void Update(const GameInterface& gameInterface);
 	void ClearResources();
 
 private:
+	//COMPONENTS
 	CubieRenderer m_cubieRenderer;
-	const InputSystem* m_inputSystem;
+	const InputSystem* inputSystem;
 
-	glm::quat m_modelRotation;
+	//CACHE
 	glm::vec2 m_previousScreenPosition;
 
-	Face m_clickedFace = UNSET;
+	//CUBE
+	glm::quat m_modelRotation;
+	std::array <std::array <std::array <Cubie*, 3>, 3>, 3> m_grid;
+	void h_RotateCube(const GameInterface& gameInterface);
 
+	//FACE ROTATION
+	CubeFace m_clickedFace = UNSET_FACE;
+	Axis m_activeFaceAxis = UNSET_AXIS;
+	int m_activeFaceSlice = UNSET_SLICE;
+	glm::vec3 m_rayCubeIntersectionPoint;
+	void h_RotateFace(const GameInterface& gameInterface);
+	void h_DetermineClickedFace(const GameInterface& gameInterface);
+	void h_DetermineActiveFace(const GameInterface& gameInterface);
 
 	//ANIMATION
 	AnimationState m_animationState = STABLE;
+	void h_UpdateAnimation(const GameInterface& gameInterface);
 
-	//HELPING METHODS
-	void h_RotateCube(const GameInterface& gameInterface);
-	void h_DetermineClickedFace(const GameInterface& gameInterface);
+	//INPUT 
+	void h_UpdateMouse(const GameInterface& gameInterface);
+	void h_UpdateKeyInput(const GameInterface& gameInterface);
 
 	//STATIC
-	const static std::map<RubicsCube::Face, glm::vec3> NORMALS_OF_FACES;
+	const static std::map<RubicsCube::CubeFace, glm::vec3> NORMALS_OF_FACES;
+	const static std::map<RubicsCube::CubeSlice, glm::vec3> NORMALS_OF_SLICES;
 
 	//DEGUB
 	LineRenderer d_lr;
